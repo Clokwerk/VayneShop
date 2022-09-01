e88fdf946dca0570a18d331f637323b461ad2e68
VaneS2 <74139708+VaneS2@users.noreply.github.com>
2022-09-02 01:07:05 +0200

    ..
    ..

--- resources/views/mpage.blade.php
+++ resources/views/mpage.blade.php
@@ -122,201 +122,44 @@
     </div>
 </header>
 
-@include('example')
+@include($page)
 
-<footer id="footer">
-    <div class="wrap-footer-content footer-style-1">
+<footer style="background-color: rgba(0, 0, 0, 0.05);" class="bg-dark border-top text-center text-lg-start   ">
 
-        <div class="wrap-function-info">
-            <div class="container">
-                <ul>
-                    <li class="fc-info-item">
-                        <i class="fa fa-truck" aria-hidden="true"></i>
-                        <div class="wrap-left-info">
-                            <h4 class="fc-name">Free Shipping</h4>
-                            <p class="fc-desc">Free On Oder Over $99</p>
-                        </div>
-
-                    </li>
-                    <li class="fc-info-item">
-                        <i class="fa fa-recycle" aria-hidden="true"></i>
-                        <div class="wrap-left-info">
-                            <h4 class="fc-name">Guarantee</h4>
-                            <p class="fc-desc">30 Days Money Back</p>
-                        </div>
-
-                    </li>
-                    <li class="fc-info-item">
-                        <i class="fa fa-credit-card-alt" aria-hidden="true"></i>
-                        <div class="wrap-left-info">
-                            <h4 class="fc-name">Safe Payment</h4>
-                            <p class="fc-desc">Safe your online payment</p>
-                        </div>
-
-                    </li>
-                    <li class="fc-info-item">
-                        <i class="fa fa-life-ring" aria-hidden="true"></i>
-                        <div class="wrap-left-info">
-                            <h4 class="fc-name">Online Suport</h4>
-                            <p class="fc-desc">We Have Support 24/7</p>
-                        </div>
-
-                    </li>
-                </ul>
-            </div>
-        </div>
-        <!--End function info-->
-
-        <div class="main-footer-content">
-
-            <div class="container">
-
-                <div class="row">
-
-                    <div class="col-lg-4 col-sm-4 col-md-4 col-xs-12">
-                        <div class="wrap-footer-item">
-                            <h3 class="item-header">Contact Details</h3>
-                            <div class="item-content">
-                                <div class="wrap-contact-detail">
-                                    <ul>
-                                        <li>
-                                            <i class="fa fa-map-marker" aria-hidden="true"></i>
-                                            <p class="contact-txt">45 Grand Central Terminal New York,NY 1017 United State USA</p>
-                                        </li>
-                                        <li>
-                                            <i class="fa fa-phone" aria-hidden="true"></i>
-                                            <p class="contact-txt">(+123) 456 789 - (+123) 666 888</p>
-                                        </li>
-                                        <li>
-                                            <i class="fa fa-envelope" aria-hidden="true"></i>
-                                            <p class="contact-txt">Contact@yourcompany.com</p>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-lg-4 col-sm-4 col-md-4 col-xs-12">
-
-                        <div class="wrap-footer-item">
-                            <h3 class="item-header">Hot Line</h3>
-                            <div class="item-content">
-                                <div class="wrap-hotline-footer">
-                                    <span class="desc">Call Us toll Free</span>
-                                    <b class="phone-number">(+123) 456 789 - (+123) 666 888</b>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="wrap-footer-item footer-item-second">
-                            <h3 class="item-header">Sign up for newsletter</h3>
-                            <div class="item-content">
-                                <div class="wrap-newletter-footer">
-                                    <form action="#" class="frm-newletter" id="frm-newletter">
-                                        <input type="email" class="input-email" name="email" value="" placeholder="Enter your email address">
-                                        <button class="btn-submit">Subscribe</button>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
-
-                    </div>
-
-                    <div class="col-lg-4 col-sm-4 col-md-4 col-xs-12 box-twin-content ">
-                        <div class="row">
-                            <div class="wrap-footer-item twin-item">
-                                <h3 class="item-header">My Account</h3>
-                                <div class="item-content">
-                                    <div class="wrap-vertical-nav">
-                                        <ul>
-                                            <li class="menu-item"><a href="#" class="link-term">My Account</a></li>
-                                            <li class="menu-item"><a href="#" class="link-term">Brands</a></li>
-                                            <li class="menu-item"><a href="#" class="link-term">Gift Certificates</a></li>
-                                            <li class="menu-item"><a href="#" class="link-term">Affiliates</a></li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="wrap-footer-item twin-item">
-                                <h3 class="item-header">Infomation</h3>
-                                <div class="item-content">
-                                    <div class="wrap-vertical-nav">
-                                        <ul>
-                                            <li class="menu-item"><a href="#" class="link-term">Contact Us</a></li>
-                                            <li class="menu-item"><a href="#" class="link-term">Returns</a></li>
-                                            <li class="menu-item"><a href="#" class="link-term">Site Map</a></li>
-                                            <li class="menu-item"><a href="#" class="link-term">Specials</a></li>
-                                            <li class="menu-item"><a href="#" class="link-term">Order History</a></li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
-
-                <div class="row">
-
-                    <div class="col-lg-4 col-sm-4 col-md-4 col-xs-12">
-                        <div class="wrap-footer-item">
-                            <h3 class="item-header">We Using Safe Payments:</h3>
-                            <div class="item-content">
-                                <div class="wrap-list-item wrap-gallery">
-                                    <img src="{{asset('assets/images/payment.png')}}" style="max-width: 260px;">
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-lg-4 col-sm-4 col-md-4 col-xs-12">
-                        <div class="wrap-footer-item">
-                            <h3 class="item-header">Social network</h3>
-                            <div class="item-content">
-                                <div class="wrap-list-item social-network">
-                                    <ul>
-                                        <li><a href="#" class="link-to-item" title="twitter"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-                                        <li><a href="#" class="link-to-item" title="facebook"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-                                        <li><a href="#" class="link-to-item" title="pinterest"><i class="fa fa-pinterest" aria-hidden="true"></i></a></li>
-                                        <li><a href="#" class="link-to-item" title="instagram"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
-                                        <li><a href="#" class="link-to-item" title="vimeo"><i class="fa fa-vimeo" aria-hidden="true"></i></a></li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-lg-4 col-sm-4 col-md-4 col-xs-12">
-                        <div class="wrap-footer-item">
-                            <h3 class="item-header">Dowload App</h3>
-                            <div class="item-content">
-                                <div class="wrap-list-item apps-list">
-                                    <ul>
-                                        <li><a href="#" class="link-to-item" title="our application on apple store"><figure><img src="{{asset('assets/images/brands/apple-store.png')}}" alt="apple store" width="128" height="36"></figure></a></li>
-                                        <li><a href="#" class="link-to-item" title="our application on google play store"><figure><img src="{{asset('assets/images/brands/google-play-store.png')}}" alt="google play store" width="128" height="36"></figure></a></li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
-            </div>
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
 
 
-        </div>
-<br>
-        <div class="coppy-right-box">
-            <div class="container">
-                <div class="coppy-right-item ">
-                    <p class="coppy-right-text text-center">VayneShop</p>
-                </div>
-
-                <div class="clearfix"></div>
-            </div>
-        </div>
     </div>
+    <!-- Grid container -->
+    <div class="container p-4">
+        <!--Grid row-->
+        <div class="row">
+            <!--Grid column-->
+            <div class="col-lg-12 col-md-12 mb-4 mb-md-0">
+                <h5 class="text-uppercase">Get it directly from the source!</h5>
+
+                <p>
+                    This app has been developed for educational purposes as a project and only contains basic functionalities necessary
+                    for the objective of the project itself. Many services can and may be developed for a more desirable
+                    application. Thank you for using VayneShop :)
+                </p>
+            </div>
+            <!--Grid column-->
+
+            <!--Grid column-->
+
+            <!--Grid column-->
+        </div>
+        <!--Grid row-->
+    </div>
+    <!-- Grid container -->
+
+    <!-- Copyright -->
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+        AgroBerza
+
+    </div>
+    <!-- Copyright -->
 </footer>
 
 <script src="{{asset('assets/js/jquery-1.12.4.minb8ff.js?ver=1.12.4')}}"></script>
