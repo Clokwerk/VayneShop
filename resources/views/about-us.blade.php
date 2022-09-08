f4b11221a19d9e64bd5b11c5f5d07eaf2619b1cc
Ana Zafirovska <ana.zafirovska@gmail.com>
2022-09-08 21:44:20 +0200

    about us
    .

--- resources/views/about-us.blade.php
+++ resources/views/about-us.blade.php
@@ -8,7 +8,7 @@
         <div class="wrap-breadcrumb">
             <ul>
                 <li class="item-link"><a href="#" class="link">home</a></li>
-                <li class="item-link"><span>Contact us</span></li>
+                <li class="item-link"><span>ABOUT US</span></li>
             </ul>
         </div>
     </div>
